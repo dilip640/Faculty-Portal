6f0da3a605e63da6ab646102c39664810ca84089
dilip640 <dilipsharma640@gmail.com>
2019-10-22 17:22:32 +0530

    Remove shutdown func

--- server/server.go
+++ server/server.go
@@ -1,10 +1,8 @@
 package server
 
 import (
-	"context"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/dilip640/Faculty-Portal/endpoints"
 	log "github.com/sirupsen/logrus"
@@ -29,23 +27,7 @@ func (s *Instance) Start() { // Startup all dependencies
 	endpoints.SetupRoutes()
 	s.httpServer = &http.Server{Addr: os.Getenv("HOST_ADDRESS"), Handler: endpoints.Router}
 	err := s.httpServer.ListenAndServe() // Blocks!
-	if err != http.ErrServerClosed {
+	if err != nil {
 		log.WithError(err).Error("Http Server stopped unexpected")
-		s.Shutdown()
-	} else {
-		log.WithError(err).Info("Http Server stopped")
-	}
-}
-
-// Shutdown shutdown the server
-func (s *Instance) Shutdown() {
-	if s.httpServer != nil {
-		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-		err := s.httpServer.Shutdown(ctx)
-		if err != nil {
-			log.WithError(err).Error("Failed to shutdown http server gracefully")
-		} else {
-			s.httpServer = nil
-		}
 	}
 }
