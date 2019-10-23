8870237d191e0b0a0aa77eed1b9e4c8c176e21ea
dilip640 <dilipsharma640@gmail.com>
2019-10-24 01:35:09 +0530

    Add password encrypt and register handler

--- auth/authHelpers.go
+++ auth/authHelpers.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/securecookie"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var cookieHandler = securecookie.New(
@@ -43,4 +45,31 @@ func clearSession(response http.ResponseWriter) {
 		MaxAge: -1,
 	}
 	http.SetCookie(response, cookie)
+}
+
+func hashAndSalt(pwd string) string {
+
+	// Use GenerateFromPassword to hash & salt pwd.
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	} // GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
+}
+
+func comparePasswords(hashedPwd, plainPwd string) bool { // Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, []byte(plainPwd))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
 }
--- auth/handler.go
+++ auth/handler.go
@@ -3,7 +3,9 @@ package auth
 import (
 	"net/http"
 
+	"github.com/dilip640/Faculty-Portal/storage"
 	"github.com/dilip640/Faculty-Portal/templatemanager"
+	log "github.com/sirupsen/logrus"
 )
 
 // HandleLogin handle the greeeting
@@ -12,15 +14,29 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/profile", 302)
 	}
 
+	data := struct {
+		Error string
+	}{}
+
 	name := r.FormValue("name")
 	pass := r.FormValue("password")
 	if name != "" && pass != "" {
-		// .. check credentials ..
-		setSession(name, w)
-		http.Redirect(w, r, "/profile", 302)
+		passwd, err := storage.CheckPasswd(name)
+		exist := comparePasswords(passwd, pass)
+		if err != nil {
+			log.Error(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if exist {
+			setSession(name, w)
+			http.Redirect(w, r, "/profile", 302)
+		} else {
+			data.Error = "Invalid Username or Password!"
+		}
 	}
 
-	templatemanager.Render(w, GetUserName(r), struct{}{}, "base", "templates/login.tpl", "templates/base.tpl")
+	templatemanager.Render(w, GetUserName(r), data, "base", "templates/login.tpl", "templates/base.tpl")
 }
 
 // HandleRegister handle the greeeting
@@ -28,16 +44,27 @@ func HandleRegister(w http.ResponseWriter, r *http.Request) {
 	if GetUserName(r) != "" {
 		http.Redirect(w, r, "/profile", 302)
 	}
+	data := struct {
+		Error string
+	}{}
 
 	name := r.FormValue("name")
 	pass := r.FormValue("password")
-	if name != "" && pass != "" {
-		// .. check credentials ..
-		setSession(name, w)
-		http.Redirect(w, r, "/profile", 302)
+	fname := r.FormValue("first_name")
+	lname := r.FormValue("last_name")
+	email := r.FormValue("email")
+	if name != "" && pass != "" && fname != "" && lname != "" && email != "" {
+		data.Error = "Enter Correct Details!"
+		err := storage.InsertEmployee(name, fname, lname, email, hashAndSalt(pass))
+		if err == nil {
+			setSession(name, w)
+			http.Redirect(w, r, "/profile", 302)
+		} else {
+			log.Error(err)
+			data.Error = "Username Taken!"
+		}
 	}
-
-	templatemanager.Render(w, GetUserName(r), struct{}{}, "base", "templates/register.tpl", "templates/base.tpl")
+	templatemanager.Render(w, GetUserName(r), data, "base", "templates/register.tpl", "templates/base.tpl")
 }
 
 // HandleLogout logout the user
