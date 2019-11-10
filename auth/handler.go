c5a18a8563052f34b6ec667ac129a3c7da609fef
jainammm <jainam.iitrpr@gmail.com>
2019-11-11 00:34:46 +0530

    Remove extra code

--- auth/handler.go
+++ auth/handler.go
@@ -15,8 +15,6 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := struct {
-		Faculty  storage.Faculty
-		Employee storage.Employee
 		Error    string
 	}{}
 
@@ -24,12 +22,13 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	pass := r.FormValue("password")
 	if name != "" && pass != "" {
 		passwd, err := storage.CheckPasswd(name)
-		exist := comparePasswords(passwd, pass)
 		if err != nil {
 			log.Error(err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
+
+		exist := comparePasswords(passwd, pass)
 		if exist {
 			setSession(name, w)
 			http.Redirect(w, r, "/profile", 302)
@@ -48,25 +47,26 @@ func HandleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := struct {
-		Faculty  storage.Faculty
-		Employee storage.Employee
 		Error    string
 	}{}
 
-	name := r.FormValue("name")
-	pass := r.FormValue("password")
-	fname := r.FormValue("first_name")
-	lname := r.FormValue("last_name")
-	email := r.FormValue("email")
-	if name != "" && pass != "" && fname != "" && lname != "" && email != "" {
-		data.Error = "Enter Correct Details!"
-		err := storage.InsertEmployee(name, fname, lname, email, hashAndSalt(pass))
-		if err == nil {
-			setSession(name, w)
-			http.Redirect(w, r, "/profile", 302)
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		pass := r.FormValue("password")
+		fname := r.FormValue("first_name")
+		lname := r.FormValue("last_name")
+		email := r.FormValue("email")
+		if name != "" && pass != "" && fname != "" && lname != "" && email != "" {
+			err := storage.InsertEmployee(name, fname, lname, email, hashAndSalt(pass))
+			if err == nil {
+				setSession(name, w)
+				http.Redirect(w, r, "/profile", 302)
+			} else {
+				log.Error(err)
+				data.Error = "Username Taken!"
+			}
 		} else {
-			log.Error(err)
-			data.Error = "Username Taken!"
+			data.Error = "Enter Correct Details!"
 		}
 	}
 	templatemanager.Render(w, GetUserName(r), data, "base", "templates/register.html", "templates/base.html")
